303a2c0797863a9f7441956498656331a066ccec
Chad Holliday <chadh@us.ibm.com>
2020-01-15 09:43:00 -0500

    add private IP output

--- terraform/aws/usecase-1-single-vm-rhel/outputs.tf
+++ terraform/aws/usecase-1-single-vm-rhel/outputs.tf
@@ -9,6 +9,11 @@ output "public_ip_address" {
   value = "${aws_instance.demo-rhel-vm.public_ip}"
 }
 
+# RHEL VM private IP address
+output "private_ip_address" {
+  value = "${aws_instance.demo-rhel-vm.private_ip}"
+}
+
 # RHEL VM instance ID
 output "instance_id" {
   value = "${aws_instance.demo-rhel-vm.id}"
